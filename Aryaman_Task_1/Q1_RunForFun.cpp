4aa99940fd2b6e5e368e6729e34228a91508273f
S1nghAryaman <aryamansinghacc@gmail.com>
2025-08-07 23:51:23 +0530

    Update Q1_RunForFun.cpp

--- Aryaman_Task_1/Q1_RunForFun.cpp
+++ Aryaman_Task_1/Q1_RunForFun.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int x,y;
+    cin >> x >> y;
+    int index = 0;
+    while(y>0){
+        y -= x;
+        index +=1;
+    }
+    cout << index-1;
+}
