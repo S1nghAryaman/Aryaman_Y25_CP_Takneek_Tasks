2b7c9bfc48a0fa289e8efa3dce594420349265b0
S1nghAryaman <aryamansinghacc@gmail.com>
2025-08-08 13:42:26 +0530

    Update Q2_ClothingStore.cpp

--- Aryaman_Task_1/Q2_ClothingStore.cpp
+++ Aryaman_Task_1/Q2_ClothingStore.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    while(n--){
+        int x,y,z,a,b,c;
+        cin >> x >> y >> z >> a >> b >> c;
+        int happy = 0;
+        if(z>c){
+            while(b>y && z != c){
+                y++;
+                z--;
+            }
+            while(a>x && z!=c){
+                x++;
+                z--;
+            }
+            while(a>x && y!=b){
+                x++;
+                y--;
+            }
+        }
+        else{
+            if(y>b){
+                while(a>x && y!=b){
+                    x++;
+                    y--;
+                }
+            }
+        }
+        happy += min(x,a) + min(y,b) + min(z,c);
+        cout << happy << endl;
 
+    }
+}
